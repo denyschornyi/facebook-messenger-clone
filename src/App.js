2ca0b983a0bcad75f1b1d6ac123155ca4df238a3
Denys Chornyi <chornijj14@gmail.com>
2020-10-09 03:37:54 +0200

    Basic structure of messanger is done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+  const [input, setInput] = useState('');
   return (
     <div className="App">
       <h1>I will get a job in this month</h1>
 
-      {/* input field */}
-      {/* button field  */}
+      <input type="text" value={input} onChange={event => setInput(event.target.value)}/>
+      <button>Send Message</button>
       {/* messages themselves */}
     </div>
   );
