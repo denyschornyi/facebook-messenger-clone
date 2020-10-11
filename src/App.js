c2e0edf330a6b27088ed27a838c59c88a958eb35
Denys Chornyi <chornijj14@gmail.com>
2020-10-11 15:24:08 +0200

    Use Form control from Material UI

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
 import Button from '@material-ui/core/Button';
+import { FormControl, InputLabel, Input } from '@material-ui/core';
 
 import './App.css';
 
@@ -7,27 +8,30 @@ function App() {
   const [input, setInput] = useState('');
   const [messages, setMessages] = useState(['a', 'b', 'c']);
 
-
-
   const sendMessege = (event) => {
     event.preventDefault();
     let inputMessage = input.trim();
     setMessages([...messages, inputMessage]);
     setInput('');
   }
+
+  
   return (
     <div className="App">
       <h1>I will get a job in this month</h1>
 
       <form>
-        <input type="text" value={input} onChange={(event) => setInput(event.target.value)}/>
-        <Button type="submit" 
+        <FormControl>
+          <InputLabel >Enter Message</InputLabel>
+          <Input value={input} onChange={(event) => setInput(event.target.value)}/>
+          <Button type="submit" 
                 variant="contained" 
                 color="primary"
                 disabled={!input}
                 onClick={(event) => sendMessege(event)}>
                     Send Message
         </Button>
+        </FormControl>
       </form>
 
       {/* messages themselves */}
